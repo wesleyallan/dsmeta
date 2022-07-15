3fed2893ad91129562b45e154d3613c14f3292d3
Wesley Silva <wesley.allansilva@gmail.com>
2022-07-15 20:14:09 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -4,11 +4,9 @@ import './styles.css'
 
 function NotificationButton() {
     return (
-        <>
         <div className="dsmeta-red-btn">
             <img src={icon} alt="Notificar" />
         </div>
-        </>
     )
 }
 
