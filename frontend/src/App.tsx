8e6a4bd1e7fe173426cf73dd7b82b054276f1475
Wesley Silva <wesley.allansilva@gmail.com>
2022-07-16 11:50:05 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
-import SalesCard from './components/SalesCard'
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton";
+import SalesCard from './components/SalesCard';
 
 function App() {
   return(
@@ -14,7 +14,7 @@ function App() {
       </section>
     </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -17,6 +17,6 @@ function Header() {
         </div>
     </header>
   );
-}
+};
 
 export default Header;
