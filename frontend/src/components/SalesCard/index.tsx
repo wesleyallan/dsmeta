af60c40c83f785b4a1446934d65e690fa5f1239b
Wesley Silva <wesley.allansilva@gmail.com>
2022-07-19 19:47:15 -0300

    fix date

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -66,7 +66,7 @@ function SalesCard() {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
-                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td className="show576">{sale.date}</td>
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
